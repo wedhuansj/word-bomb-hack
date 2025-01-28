4a559f7897ea3eea51895c71c50122cac95d0aec
wedhuansj <nguyenbuihongquan@gmail.com>
2025-01-28 13:32:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import pyautogui, random, time, keyboard
-word_list_path='wordlist.txt'
-with open(word_list_path,'r',encoding='utf-8')as f:word_list=[line.strip()for line in f]
-random.shuffle(word_list)
-used_words=set()
-def find_unique_word(prompt):
- for word in[w for w in word_list if w not in used_words]:
-  if prompt in word:used_words.add(word);return word
- return None
+import pyautogui as a, random as b, time as c, keyboard as d
+e='wordlist.txt'
+with open(e,'r',encoding='utf-8')as f:g=[line.strip()for line in f]
+b.shuffle(g)
+h=set()
+def i(j):
+    for k in[w for w in g if w not in h]:
+        if j in k:h.add(k);return k
+    return None
 print("Program is running. Type a prompt and press enter. Press Ctrl+C to stop.")
 try:
- while True:
-  prompt=input().strip().lower()
-  if prompt=="":used_words.clear();print("Used words have been reset.");continue
-  matched_word=find_unique_word(prompt)
-  if matched_word:
-   print(f"Matched word: {matched_word}")
-   pyautogui.hotkey('alt','tab')
-   time.sleep(0.3)
-   for letter in matched_word:
-    if random.random()<0.1:
-     keyboard.write(chr(ord(letter)+1))
-     time.sleep(0.3) # Time to wait before typing backspace (this is adjustable)
-     keyboard.press_and_release('backspace')
-    keyboard.write(letter)
-    time.sleep(random.uniform(0.07,0.09)) # Typing speed (this is adjustable)
-   keyboard.press_and_release('enter')
-   time.sleep(0.3)
-   pyautogui.hotkey('alt','tab')
-  else:print("No unique word found for the given prompt.") 
+    while True:
+        l=input().strip().lower()
+        if l=="":h.clear();print("Used words have been reset.");continue
+        m=i(l)
+        if m:
+            print(f"Matched word: {m}")
+            a.hotkey('alt','tab')
+            c.sleep(0.3)
+            for n in m:
+                if b.random()<0.1:
+                    d.write(chr(ord(n)+1))
+                    c.sleep(0.3) # Time to wait before typing backspace (this is adjustable)
+                    d.press_and_release('backspace')
+                d.write(n)
+                c.sleep(b.uniform(0.07,0.09)) # Typing speed (this is adjustable)
+            d.press_and_release('enter')
+            c.sleep(0.3)
+            a.hotkey('alt','tab')
+        else:print("No unique word found for the given prompt.")
 except KeyboardInterrupt:print("Program terminated by user.")
